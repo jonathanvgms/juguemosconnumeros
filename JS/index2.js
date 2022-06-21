61ea80234464ba0ef188c117487117c7959ad1c7
LeonFBeymar <beymarleonf18@gmail.com>
2022-06-21 02:21:02 -0300

    MODIFICACIONES Y FINALIZACION DE LOS RETOS SUMA RESTA

---
+++ JS/index2.js
@@ -0,0 +1,69 @@
+var sound = new Audio();
+    sound.src = "./SOUND/correcto.mp3";
+
+var soundError = new Audio();
+    soundError.src = "./SOUND/incorrecto.mp3";
+
+function cambiar2(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./retosumaresta2.html";
+    }, 1000);
+}
+
+function cambiar3(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./ retosumaresta3.html";
+    }, 1000);
+}
+function cambiar4(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./ retosumaresta4.html";
+    }, 1000);
+}
+function cambiar5(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./ retosumaresta5.html";
+    }, 1000);
+}
+function cambiar6(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./ retosumaresta6.html";
+    }, 1000);
+}
+function cambiar7(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./ retosumaresta7.html";
+    }, 1000);
+}
+function cambiar8(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./ retosumaresta8.html";
+    }, 1000);
+}
+function cambiar9(){
+    
+    sound.play();
+    setTimeout(() => {
+        location.href = "./ retosumaresta9.html";
+    }, 1000);
+}
+function cambiarOtro() {
+    sound.play();
+    setTimeout(() => {
+        location.href = "./menu.html"
+    }, 1000);
+}
